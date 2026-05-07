b87dbef2cac53a8b5a797d26fe6c3604592ddda7
Sahil Nihalani <sahilnihalani0@gmail.com>
2026-05-07 19:22:45 +0530

    chore: make ecr safer by enabling image scanning & immutablity

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,6 +1,9 @@
 resource "aws_ecr_repository" "apps" {
   for_each             = toset(["example-vote-app", "example-result-app", "example-worker-app"])
   name                 = each.value
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   force_delete         = true
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }

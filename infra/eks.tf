8087cb1def6db498b030e2408859c442825f945f
Sahil Nihalani <sahilnihalani0@gmail.com>
2026-05-07 16:07:24 +0530

    fix: deprecated values

--- infra/eks.tf
+++ infra/eks.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_prefix_list" "s3" {
-  name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  name = "com.amazonaws.${data.aws_region.current.region}.s3"
 }
 
 module "eks" {
